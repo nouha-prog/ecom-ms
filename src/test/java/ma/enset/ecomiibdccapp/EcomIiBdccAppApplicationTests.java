0fe201fe51e8126166651186559b2e6a14e7d2a3
Nouhaila <nouhailaelhajji488@gmail.com>
2025-11-02 16:06:49 +0100

    Adding project root

---
+++ src/test/java/ma/enset/ecomiibdccapp/EcomIiBdccAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.ecomiibdccapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomIiBdccAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
