b12ca60ccea1d400d7ff65d512203d14a58e0939
Nouhaila <nouhailaelhajji488@gmail.com>
2025-11-16 15:22:21 +0100

    add ConfigTestRestController and CustomerConfigParams and modifying CustomerSrviceAplication to handle customer configuration properties

--- customer-service/src/main/java/ma/enset/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/ma/enset/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package ma.enset.customerservice;
 
 import ma.enset.customerservice.entities.Customer;
+import ma.enset.customerservice.config.CustomerConfigParams;
 import ma.enset.customerservice.repository.CustomerRepository;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
